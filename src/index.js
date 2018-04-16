ef31a252268bc9a85e224e3b7256c02efe4fc3d2
Gustly7 <Gustly@yandex.ru>
2018-04-16 17:23:38 +0400

    commit 003

--- src/index.js
+++ src/index.js
@@ -9,46 +9,38 @@ import registerServiceWorker from './registerServiceWorker';
 //ReactDOM.render(<App />, document.getElementById('root'));
 //registerServiceWorker();
 
-function tick(){
-    const elem = (
-            <div>
-                <h1> Hello</h1>
-                <h2>Time is {new Date().toLocaleTimeString()}</h2>
-            </div>
-        );
-    ReactDOM.render(
-        elem,
-        document.getElementById('root')
-    )
-}
+// function tick(){
+//     const elem = (
+//             <div>
+//                 <h1> Hello</h1>
+//                 <h2>Time is {new Date().toLocaleTimeString()}</h2>
+//             </div>
+//         );
+//     ReactDOM.render(
+//         elem,
+//         document.getElementById('root')
+//     )
+// }
 //setInterval(tick,1000);
-
-var elem = React.createElement('div',{},'<h1> Hello, i\'m "React.createElement" component!&#128005;</h1>');
-
-ReactDOM.render(
-    elem,
-    document.getElementById('root')
-);
-
-//React.Component
-ReactDOM.render(
-    <HelloClass/>,
-    document.getElementById('root2')
-);
-
-//React.PureComponent
-ReactDOM.render(
-    <PureComp/>,
-    document.getElementById('root3')
-);
 
 //functional component
 function HelloFunc(props) {
     return <h1> Hello, {props.name}. I'm a functional component</h1>
 }
 
-const elemFunc = <HelloFunc name = "User" />;
+var elem = React.createElement('div',{},
+    //React.createElement('h1', {}, 'Hello, i\'m "React.createElement" component! &#128005;')
+    React.createElement('h1', {}, <div dangerouslySetInnerHTML= {{__html:'Hello, i\'m "React.createElement" component! &#128269;'}} />)
+);
+
+const MainElem = React.createElement('div',{},
+    elem,
+    <HelloClass/>,
+    <PureComp/>,
+    <HelloFunc name = "User" />
+);
+
 ReactDOM.render(
-    elemFunc,
-    document.getElementById('root4')
+    MainElem,
+    document.getElementById('root')
 );
